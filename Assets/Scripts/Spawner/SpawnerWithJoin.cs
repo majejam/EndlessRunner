ef82b55919a9b792d9c66ca8b8ba1f251b61c92e
majejam <t.f.m.lacroix@gmail.com>
2021-01-28 17:53:45 +0100

    :sparkles: added join trigger

---
+++ Assets/Scripts/Spawner/SpawnerWithJoin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerWithJoin : MonoBehaviour
+{
+    public GameObject[] ObjectPrefab;
+    public GameObject LastObject;
+
+    public void Spawn()
+    {
+        int index = Random.Range(0, ObjectPrefab.Length);
+        GameObject copy = Instantiate(ObjectPrefab[index]);
+
+        Vector3 pos = LastObject.transform.position;
+
+        
+
+        pos.z = pos.z - copy.GetComponent<BoxCollider>().bounds.size.z;
+
+        copy.transform.position = pos;
+
+        LastObject = copy;
+    }
+}
